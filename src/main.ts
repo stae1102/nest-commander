819ea6734f385d60577851bf279a2f91a80d1e57
Seongtae <stae1102@gmail.com>
2023-02-14 17:29:22 +0900

    feat: commander에 로거와 에러 핸들러를 추가한다

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { CommandFactory } from 'nest-commander';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  await CommandFactory.run(AppModule);
+  await CommandFactory.run(AppModule, {
+    logger: new Logger(),
+    errorHandler: (error) => {
+      console.error(error);
+      process.exit(1);
+    },
+  });
 }
 bootstrap();
